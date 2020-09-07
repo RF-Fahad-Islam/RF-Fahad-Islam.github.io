3fb62234ac6bc521a1a2d4900b344d8e30ab0dc1
RF Fahad <68589534+RF-Fahad-Islam@users.noreply.github.com>
2020-09-07 21:02:00 +0600

    Add files via upload

--- alarm.js
+++ alarm.js
@@ -15,7 +15,6 @@ function setAlarm(e) {
     e.preventDefault();
     let alarmTime = new Date(alarm.value);
     let now = new Date();
-    alarmHelp.innerHTML = `<b style="color: green;">Setting alarm on ${alarmTime}</b>`
     console.log(`Setting alarm on ${alarmTime}...`);
     let leftTime = alarmTime - now;
     console.log(`Left time ${leftTime}`);
@@ -27,12 +26,12 @@ function setAlarm(e) {
             audioPlay();
         }, leftTime);
         alarmHelp.innerHTML = `<b style="color: green;">Alarm successfully set. show details on status bar.</b>`
-        message.innerHTML = `<b style="color: green;">Alarm will ringing on ${alarmTime}</b>`
+        message.innerHTML = `<b style="color: green;">Alarm will ringing on ${alarmTime.toLocaleDateString()}<hr>Time: ${alarmTime.toLocaleTimeString()}</b><hr>`
         alarm.classList.remove('is-invalid');
         alarm.classList.add('is-valid');
     } else {
         alarmHelp.innerHTML = `<b style="color: red;">Time is Invalid. Set a future Time.</b>`
-        message.innerHTML = `<b style="color: red;">Cannot set alarm on.${alarmTime}</b>`
+        message.innerHTML = `<b style="color: red;">Cannot set alarm on: ${alarmTime}</b>`
         alarm.classList.add('is-invalid');
         alarm.classList.remove('is-valid');
         status.innerHTML = `<b style="color: red; background-color: white;">Error! Time cannot display. {>.._..<}`
@@ -49,12 +48,12 @@ function statusBar(time) {
                 leftTime = time - now;
                 let second = leftTime / 1000;
                 status.innerHTML = `<b>Time left: ${second}s</b>`
-            }, 1000);
+            }, 100);
         } else {
             status.innerHTML = `<b>Time Left: 0s;</b>`
         }
 
-    }, 1000)
+    }, 100)
 }
 
 function audioPlay() {
