9f3755f48277d279fc9a0aca1e25ceb99913c80d
Fahad <68589534+RF-Fahad-Islam@users.noreply.github.com>
2021-09-02 13:03:57 +0600

    Add files via upload

--- numberguessgame.js
+++ numberguessgame.js
@@ -1,31 +1,48 @@
-console.log("Hello")
+// console.log("Hello")
 let num = Math.random()
-console.log(num);
-startNum = 1
-endNum = 100
-num = Math.round(startNum + (endNum - startNum) * num)
+// console.log(num);
+let startNum;
+let endNum;
 let submitBtn = document.getElementById('submitBtn')
-let inputNum = 0;
+let inputNum;
+startNum = 1
 let iteration = 1;
+let level;
 let numState = document.getElementById('numState')
 let playAgainBtn = document.getElementById('playAgainBtn')
 let ansColor = document.getElementById('ansColor')
 let form = document.getElementById("form")
+let startGame = document.getElementById("startGame")
+let startNumPage = document.getElementById("startNum")
+let endNumPage = document.getElementById("endNum")
+var myModal = new bootstrap.Modal(document.getElementById('modalLevel'))
+myModal.show()
 form.addEventListener("submit", (e) => {
     e.preventDefault()
 })
-playAgainBtn.addEventListener('click', e=> {
+playAgainBtn.addEventListener('click', e => {
     location.reload()
 })
-console.log(num);
+// console.log(num);
+startGame.addEventListener('click', e => {
+    e.preventDefault()
+    endNum = Number(document.querySelector("input[name='level']:checked").value)
+    num = Math.round(startNum + (endNum - startNum) * num)
+    endNumPage.innerText = String(endNum)
+    startNumPage.innerText = String(startNum)
+    myModal.hide()
+})
+
 submitBtn.addEventListener('click', function (e) {
     inputNum = Number(document.getElementById('inputNum').value)
-    console.log(inputNum);
+    // console.log(inputNum);
 
     if (inputNum < num) {
-        numState.innerHTML = `The number is greater than ${inputNum}`;
+        numState.innerHTML = `The number is greater than <strong>${inputNum}</strong>`;
+        ansColor.classList = 'alert alert-danger text-center'
     } else if (inputNum > num) {
-        numState.innerHTML = `The number is less than ${inputNum}`;
+        numState.innerHTML = `The number is less than <strong>${inputNum}</strong>`;
+        ansColor.classList = 'alert alert-danger text-center'
     } else if (inputNum === num) {
         numState.innerHTML = `<strong>Well Done!</strong> You guess the number by "${iteration}" try`;
         playAgainBtn.style.display = "inline-block"
