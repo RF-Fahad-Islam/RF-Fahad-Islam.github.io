5785492efa6e0e0683968d5fd4db43487dd1e48c
Fahad <68589534+RF-Fahad-Islam@users.noreply.github.com>
2021-09-02 16:17:59 +0600

    Add files via upload

--- numberguessgame.js
+++ numberguessgame.js
@@ -13,9 +13,21 @@ let playAgainBtn = document.getElementById('playAgainBtn')
 let ansColor = document.getElementById('ansColor')
 let form = document.getElementById("form")
 let startGame = document.getElementById("startGame")
-let startNumPage = document.getElementById("startNum")
-let endNumPage = document.getElementById("endNum")
+let guessFrom = document.getElementById("guessFrom")
+let customBox = document.getElementById("customBox")
 var myModal = new bootstrap.Modal(document.getElementById('modalLevel'))
+let radioBtns = document.querySelectorAll("input[name='level']")
+
+for (radioBtn of radioBtns) {
+    radioBtn.addEventListener("click", (e) => {
+        if (e.target.value === "custom") {
+            customBox.style.display = "block"
+        } else {
+            customBox.style.display = "none"
+
+        }
+    })
+}
 myModal.show()
 form.addEventListener("submit", (e) => {
     e.preventDefault()
@@ -26,11 +38,21 @@ playAgainBtn.addEventListener('click', e => {
 // console.log(num);
 startGame.addEventListener('click', e => {
     e.preventDefault()
-    endNum = Number(document.querySelector("input[name='level']:checked").value)
-    num = Math.round(startNum + (endNum - startNum) * num)
-    endNumPage.innerText = String(endNum)
-    startNumPage.innerText = String(startNum)
-    myModal.hide()
+    if (document.querySelector("input[name='level']:checked").value == "custom") {
+        startNum = Number(document.getElementById("customStartNum").value)
+        endNum = Number(document.getElementById("customEndNum").value)
+    } else {
+        startNum = 1
+        endNum = Number(document.querySelector("input[name='level']:checked").value)
+    }
+    if (endNum == 0) {
+        alert("please enter End Number")
+    } else {
+        num = Math.round(startNum + (endNum - startNum) * num)
+        guessFrom.innerHTML = `	
+        <i class="fa fa-info-circle"></i> <strong>${startNum}-${endNum}</strong>`
+        myModal.hide()
+    }
 })
 
 submitBtn.addEventListener('click', function (e) {
@@ -38,13 +60,13 @@ submitBtn.addEventListener('click', function (e) {
     // console.log(inputNum);
 
     if (inputNum < num) {
-        numState.innerHTML = `The number is greater than <strong>${inputNum}</strong>`;
+        numState.innerHTML = `The number is greater than <strong>${inputNum}</strong> <hr><i class="fa fa-exclamation-triangle"></i> Try : ${iteration}`;
         ansColor.classList = 'alert alert-danger text-center'
     } else if (inputNum > num) {
-        numState.innerHTML = `The number is less than <strong>${inputNum}</strong>`;
+        numState.innerHTML = `The number is less than <strong>${inputNum}</strong> <hr> <i class="fa fa-exclamation-triangle"></i> Try : ${iteration}`;
         ansColor.classList = 'alert alert-danger text-center'
     } else if (inputNum === num) {
-        numState.innerHTML = `<strong>Well Done!</strong> You guess the number by "${iteration}" try`;
+        numState.innerHTML = `<i class="fa fa-check-circle"></i> <strong>Well Done!</strong> You guess the number by "${iteration}" try`;
         playAgainBtn.style.display = "inline-block"
         ansColor.classList = 'alert alert-success text-center'
     } else {
